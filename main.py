a97fc37022ed2b45f82217305067afc77f26301a
AITU homework <https://github.com/cyberkuzetshi/Aitu.git>
2022-11-01 02:53:58 +0600

    hw

--- main.py
+++ main.py
@@ -1,33 +1,108 @@
-import datetime as dt
-import requests
+board_list = list(range(1,10))
 
-BASE_URL = 'http://api.openweathermap.org/data/2.5/weather?'
-API_KEY = '6b0769274692e24c0ca7c748e2a0e0ec'
-CITY = 'Astana'
+def draw_board(board_list):
+    print ("-------------")
+    for i in range(3):
+        print ("|", board_list[0+i*3], "|", board_list[1+i*3], "|", board_list[2+i*3], "|")
+        print ("-------------")
 
-def kelvin_to_celcius_fahrenheit(kelvin):
-    celcius = kelvin - 273.15
-    fahrenheit = celcius * (9/5) + 32
-    return celcius, fahrenheit
+def put(board_list):
+    counter = 0
+    x = 9
+    win = True
+    while win:
+        if counter % 2 == 0:
+            print('Select the index to put X')
+            draw_board(board_list)
+            index = int(input())
+            for i in range(x):
+                if board_list[index-1] == index:
+                    board_list.pop(index-1)
+                    board_list.insert(index-1, 'X')
+                    draw_board(board_list)
+                    break;
+                elif board_list[index-1] == 'X':
+                    print('Occupied. Select other one.')
+                    put(board_list)
+                else:
+                    print('No. From 1 to 9')
+                    put(board_list)
+            counter += 1
 
-url = BASE_URL + 'appid=' + API_KEY + '&q=' + CITY
+        else:
+            print('Select the index to put Y')
+            draw_board(board_list)
+            index = int(input())
+            for i in range(x-1):
+                if board_list[index-1] == index:
+                    board_list.pop(index-1)
+                    board_list.insert(index-1, 'Y')
+                    draw_board(board_list)
+                    break;
+                elif board_list[index-1] == 'Y':
+                    print('Occupied. Select other one.')
+                    put(board_list)
+                else:
+                    print('No. From 1 to 9')
+                    put(board_list)
+            counter += 1
 
-responce = requests.get(url).json()
+        if counter > 3:
+            if diagonal(board_list) == 'X win by diagonal' or straight(board_list) == 'X win by line':
+                print('X win')
+                win = False
+                break;
+            if diagonal(board_list) == 'Y win by diagonal' or straight(board_list) == 'Y win by line':
+                print('Y win')
+                win = False
+                break;
+        if counter == 9:
+            print('friendship')
+            win = False
+            break;
 
-temp_kelvin = responce['main']['temp']
-temp_celcius, temp_fahrenheit = kelvin_to_celcius_fahrenheit(temp_kelvin)
-feels_like_kelvin = responce['main']['feels_like']
-feels_like_celsius, feels_like_fahrenheit = kelvin_to_celcius_fahrenheit(feels_like_kelvin)
-wind_speed = responce['wind']['speed']
-humidity = responce['main']['humidity']
-description = responce['weather'][0]['description']
-sunrise_time = dt.datetime.utcfromtimestamp(responce['sys']['sunrise'] + responce['timezone'])
-sunset_time = dt.datetime.utcfromtimestamp(responce['sys']['sunset'] + responce['timezone'])
 
-print(f"Temp in {CITY}: {temp_celcius:.2f} or {temp_fahrenheit:.2f}")
-print(f"Temp in {CITY} feels like: {feels_like_celsius:.2f} or {feels_like_fahrenheit:.2f}")
-print(f"Humidity in {CITY}: {humidity}%")
-print(f"Wind Speed in {CITY}: {wind_speed}m/s")
-print(f"General weather in {CITY}: {description}")
-print(f"Sun rises in {CITY} at {sunrise_time} local time")
-print(f"Sun sets in {CITY} at {sunset_time} local time")
+def diagonal(board_list):
+    #di = True
+    #for i in range(3):
+    if board_list[0] == 'X' and  board_list[4]== 'X' and  board_list[8]== 'X':
+       return 'X win by diagonal'
+    if board_list[2] == 'X' and  board_list[4]== 'X' and  board_list[6]== 'X':
+        return 'X win by diagonal'
+    if board_list[2] == 'Y' and  board_list[4]== 'Y' and  board_list[6]== 'Y':
+        return 'Y win by diagonal'
+    if board_list[0] == 'Y' and board_list[4] == 'Y' and board_list[8] == 'Y':
+        return 'Y win by diagonal'
+    return None;
+
+def straight(board_list):
+   # str = True
+    if board_list[0]=='X' and board_list[1] =='X' and board_list[2]=='X':
+        return 'X win by line'
+    if board_list[3] =='X' and board_list[4]=='X' and board_list[5]=='X':
+         return 'X win by line'
+    if board_list[6] == 'X' and board_list[7] == 'X' and board_list[8] == 'X':
+        return 'X win by line'
+    if board_list[0] == 'X' and board_list[3] == 'X' and board_list[6] == 'X':
+        return 'X win by line'
+    if board_list[1] == 'X' and board_list[4] == 'X' and board_list[7] == 'X':
+        return 'X win by line'
+    if board_list[3] == 'X' and board_list[5] == 'X' and board_list[8] == 'X':
+        return 'X win by line'
+
+    if board_list[0] == 'Y' and board_list[1] == 'Y' and board_list[2] == 'Y':
+        return 'Y win by line'
+    if board_list[3] == 'Y' and board_list[4] == 'Y' and board_list[5] == 'Y':
+        return 'Y win by line'
+    if board_list[6] == 'Y' and board_list[7] == 'Y' and board_list[8] == 'Y':
+         return 'Y win by line'
+    if board_list[0] == 'Y' and board_list[3] == 'Y' and board_list[6] == 'Y':
+        return 'Y win by line'
+    if board_list[1] == 'Y' and board_list[4] == 'Y' and board_list[7] == 'Y':
+        return 'Y win by line'
+    if board_list[2] == 'Y' and board_list[5] == 'Y' and board_list[3] == 'Y':
+        return 'Y win by line'
+
+    return None;
+
+put(board_list)
